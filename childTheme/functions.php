6b1e3e98696f81a0781e466aab3496d6f3ecb3d3
Sai Chandan <saichandan@Sais-MacBook-Air.local>
2023-02-03 09:29:28 +0530

    Hooks implementation

--- childTheme/functions.php
+++ childTheme/functions.php
@@ -26,3 +26,61 @@ function custom_sidebars() {
 }
 add_action( 'widgets_init', 'custom_sidebars' );
 ?>
+
+
+<?php
+
+/* USING HOOKS */
+
+    add_action('save_post', 'log_when_saved'); //USING ACTION HOOK WHEN POST IS SAVED
+
+    function log_when_saved( $post_id ){ //CUSTOM FUNCTION TO HOOK TO AN ACTION
+        
+        if( !(wp_is_post_revision($post_id)) || wp_is_post_autosave($post_id)){ //CONDTION TO CHECK IF POST IS AUTO SAVED OR DRAFTED
+            return;
+        }
+
+        
+
+        $post_log = get_stylesheet_directory().'/post_log.txt'; //GET PATH OF POST_LOG TEXT FILE
+        $msg = get_the_title( $post_id ).' was saved';
+
+        if (file_exists($post_log)){ //CONDITION TO CHECK FILE EXISTS OR NOT
+            $file = fopen($post_log, 'a');
+            fwrite($file, $msg." ");
+        }
+        else
+        {
+            $file = fopen($post_log, 'w');
+            fwrite($file, $msg." ");
+        }
+
+        fclose($file);
+        do_action('get_time', date("F j, Y, g:i a")); //CALL TO OUR ACTION HOOKED FUNCTION AND PASS DATE AS PARAM
+    }
+
+    add_action('get_time', 'log_time');
+
+    function log_time( $time ){
+        $post_log = get_stylesheet_directory().'/post_log.txt';
+        $msg = apply_filters('log_msg_using_filter', 'and filter hook executed at '.$time); //CALL TO OUR FILTER HOOKED FUNCTION
+
+        if (file_exists($post_log)){
+            $file = fopen($post_log, 'a');
+            fwrite($file, $msg."\n");
+        }
+        else
+        {
+            $file = fopen($post_log, 'w');
+            fwrite($file, $msg."\n");
+        }
+        
+    }
+
+    add_filter('log_msg_using_filter', function( $msg ){ //FILTER HOOK
+        return " ".$msg." ";
+    });
+?>
+
+
+
