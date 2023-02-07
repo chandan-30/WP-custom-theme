87f5c1ea5a6feed5621149fdafd88c8354a60ec3
Sai Chandan <saichandan@Sais-MacBook-Air.local>
2023-02-07 12:14:49 +0530

    commiting custom taxonomy code

--- childTheme/functions.php
+++ childTheme/functions.php
@@ -25,3 +25,39 @@ function custom_sidebars() {
 }
 add_action( 'widgets_init', 'custom_sidebars' );
 ?>
+
+<?php 
+    /*
+* Plugin Name: Topic Taxonomy
+* Description: Topic Taxonomy.
+* Version: 1.0
+* Author: Chandan
+* Author URI: ''
+*/
+
+function wporg_register_taxonomy_Topic() {
+    $labels = array(
+        'name'              => _x( 'Topics', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Topic', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Topics' ),
+        'all_items'         => __( 'All Topics' ),
+        ''       => __( 'Parent Topic' ),
+        'parent_item_colon' => __( 'Parent Topic:' ),
+        'edit_item'         => __( 'Edit Topic' ),
+        'update_item'=> __( 'Update Topic' ),
+        'add_new_item'      => __( 'Add New Topic' ),
+        'new_item_name'     => __( 'New Topic Name' ),
+        'menu_name'         => __( 'Topic' ),
+    );
+    $args   = array(
+        'hierarchical'      => true, // make it hierarchical (like categories)
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => [ 'slug' => 'Topic' ],
+    );
+    register_taxonomy( 'Topic', [ 'news' ], $args );
+}
+add_action( 'init', 'wporg_register_taxonomy_Topic' );
+?>
